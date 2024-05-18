8a7d0b543dbfe68a04862fe34630a2fb6dd01318
Le Ba Toan <toanlb.bhsoft@gmail.com>
2024-05-18 23:56:05 +0700

    update

--- back-end/routes/user/index.js
+++ back-end/routes/user/index.js
@@ -4,23 +4,34 @@ const router = express.Router();
 
 router.get('/', function (request, response) {
   const query = request.query;
-  let email = query.email || '';
-  let name = query.name || '';
-  database.query(
-    'SELECT * FROM user WHERE email LIKE ? AND fullname LIKE ? and user.permision = 1',
-    [`%${email}%`, `%${name}%`],
-    function (error, results) {
-      if (error) throw error;
-      const result = results.map((user) => {
-        return {
-          id: user.id,
-          email: user.email,
-          name: user.fullname,
-        };
-      });
-      return response.json(result);
-    },
-  );
+  let email = query.email;
+  let name = query.name;
+  let sql = 'SELECT * FROM user';
+  const whereSql = [];
+  if (email) {
+    whereSql.push(`user.email LIKE '%${email}%'`);
+  }
+  if (name) {
+    whereSql.push(`user.fullname LIKE '%${name}%'`);
+  }
+
+  if (whereSql.length > 0) {
+    sql += ' WHERE ' + whereSql.join(' AND ') + 'and user.permision = 1';
+  } else {
+    sql += ' where user.permision = 1';
+  }
+
+  database.query(sql, [], function (error, results) {
+    if (error) throw error;
+    const result = results.map((user) => {
+      return {
+        id: user.id,
+        email: user.email,
+        name: user.fullname,
+      };
+    });
+    return response.json(result);
+  });
 });
 
 module.exports = router;
